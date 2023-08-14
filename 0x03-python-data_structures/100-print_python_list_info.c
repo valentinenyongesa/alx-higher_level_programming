bca4868d5f2a3fa9aa5c829a8fca2b4e0257e44c
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-08-14 10:24:03 -0700

    Prints some basic info about Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -18,7 +18,8 @@ void print_python_list_info(PyObject *p)
 	for (i = 0; i < size; i++)
 	{
 		printf("Element %d:", i);
-		obj = PyList_Get|temp(p, i);
+
+		obj = PyList_GetItem(p, i);
 		printf("%s\n", Py_TYPE(obj)->tp_name);
 	}
 }
