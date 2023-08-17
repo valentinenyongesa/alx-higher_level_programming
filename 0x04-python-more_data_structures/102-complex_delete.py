38acc041d694a6f164a1413233b326d81db5fe2f
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-08-17 04:38:39 -0700

    Deletes keys with a specific value in a dictionary

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    """Deletes keys with a specific value in a dictionary"""
+    list_keys = list(a_dictionary.keys())
+
+    for value_dic in list_keys:
+        if value == a_dictionary.get(value_dic):
+            del a_dictionary[value_dic]
+
+    return (a_dictionary)
