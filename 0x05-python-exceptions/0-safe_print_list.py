2739929d8a2226749d955ca2f5cc46d13ce35e7d
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-08-28 05:29:11 -0700

    Prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """Prints x elements of a list and
+    Returns the real number of elements printed"""
+    no_elements = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            no_elements += 1
+        except indexError:
+            break
+    print("")
+    return(no_elements)
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,2 @@
+0x05. Python - Exceptions
+Handles errors and exceptons
