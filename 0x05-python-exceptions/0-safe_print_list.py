ca0109a0bf1b2a4dc1c48a2668aac80ec85e7ca9
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-08-28 06:00:59 -0700

    prints an integer with "{:d}".format()

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,9 @@ def safe_print_list(my_list=[], x=0):
     no_elements = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end=" ")
             no_elements += 1
         except indexError:
             break
-    print("")
+    print(" ")
     return(no_elements)
