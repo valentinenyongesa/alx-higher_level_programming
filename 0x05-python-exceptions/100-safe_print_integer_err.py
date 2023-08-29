64701c91c3767263b60d92553588ce8759156c8a
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-08-28 23:33:55 -0700

    Prints an integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_print_integer_err(value):
+    """Prints integer with "{:d}".format().
+
+    If a value error message is incorrectly printed
+    prints in stderr the error
+
+    Args:
+        value(int): integer to print
+    Returns:
+        if error occurs - False
+        otherwise - True"""
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception:{}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
