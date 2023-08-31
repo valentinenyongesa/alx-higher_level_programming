e038a967cd11e0084a186bb303546305b2dd5563
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-08-30 17:46:14 -0700

    Defines class square wit private instance size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines a squre based on 0-square.py"""
+
+
+class Square:
+
+    """Private instance attribute: size"""
+    def __init__(self, size):
+        """Defines size as a private instance
+
+        Args:
+            size (int): size of latest square.
+        """
+    self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+"""Defines a squre based on 0-square.py"""
+class Square:
+    """Private instance attribute: size"""
+    self.__size = size
