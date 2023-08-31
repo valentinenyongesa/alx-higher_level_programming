5fade678ae7c8ba13824dcb474354971ad981d86
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-08-30 18:09:47 -0700

    Defines a class square with a private instance and instantation with optional size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
 """Defines a squre based on 0-square.py"""
+
+
 class Square:
     """Private instance attribute: size"""
-    self.__size = size
+
+    def __init__(self, size=0):
+        """initialize new square
+
+        Args:
+            size (int): size of new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
