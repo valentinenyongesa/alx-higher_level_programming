f914d516777722a1aa1d58c3eecd0fc342141b9f
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-09-04 10:52:03 -0700

    Write a class with private attributes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Defines a class rectangle with instances"""
+
+
+class Rectangle:
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new rectangle
+
+        Args:
+            width(int): width of new rectangle
+            height(int): height of new rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """set width of rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """set height of a rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
