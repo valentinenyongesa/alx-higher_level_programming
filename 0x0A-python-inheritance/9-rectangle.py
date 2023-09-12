11a58c1eef6ecb34091d8ee44db839184bbfb493
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-09-12 02:40:41 -0700

    Write a class Rectangle that inherits from BaseGeometry

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -24,6 +24,6 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Return print() and str() representation of a rectangle"""
-        string = "[" + str(self.__class__.__name__) + "]"
+        string = "[" + str(self.__class__.__name__) + "] "
         string += str(self.__width) + "/" + str(self.__height)
         return string
