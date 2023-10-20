636fbcbdce7908e32fe36966e558409c27838366
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-10-19 18:09:30 -0700

    JOIN statements

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,3 @@
+-- Lists all shows contained in hbtn_0d_tvshows without a genre linked
+-- Lists all records that dont have one column
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
