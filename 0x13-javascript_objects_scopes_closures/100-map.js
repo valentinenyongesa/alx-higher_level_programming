176559bb46c98083ccf2325d35cc177d8b19f6a4
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-11-15 18:20:15 -0800

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-let narg = 0;
-
-exports.logMe = function (item) {
-  console.log(narg + ': ' + item);
-  narg++;
-};
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
