6578588a546bb7efd71c7d00a7bded33079b8f63
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-11-30 04:09:15 -0800

    Find peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+    """Get a peak in list of unsorted integers"""
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
