a225e24ef522d4a758459c1fbc09ab310d55139e
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-11-30 00:08:13 -0800

    cURL body size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#takes in a URL, sends a request to that URL, and displays the size
+
+
+curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r'
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,7 @@
+0x10. Python - Network #0
+This project handles concepts such as
+1. Bash
+2. Python
+3. Scripting
+4. Back-end
+5. API
