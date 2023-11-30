a45f1c0ea4da009e49cf8654f470d8540e551869
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-11-30 00:15:51 -0800

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-#takes in a URL, sends a request to that URL, and displays the size
-
-
+#takes in a URL, sends a request to that URL, and displays the body size
 curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r'
