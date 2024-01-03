dcc9606b912f46b4efd8b9259d1e3812a11519d2
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-01-03 04:49:16 -0800

    Reads and prints contents of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 // imports the built-in Node.js module fs (File System)
 
-fs.readFile(process.argv[2], 'utf-8', (error, content) {
+fs.readFile(process.argv[2], 'utf-8', (error, content) => {
   if (error) {
     console.error('Error reading the file:', error);
   } else {
