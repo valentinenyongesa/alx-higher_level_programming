77f405b94c91eff732f9b8e706315a2be79c12b9
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-01-03 05:21:34 -0800

    Display status code of a GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+// imports the built-in Node.js module request (File System)
+
+request.get(process.argv[2])
+  .on('response', function (response) {
+
+    console.log(`code: ${response.statusCode}`);
+ });
