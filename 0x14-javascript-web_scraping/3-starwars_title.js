abce5fabb0be53edfc46041fe8fcb12a43439527
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-01-03 05:37:37 -0800

    Prints the title of a Star Wars movie where the episode number matches a given integer

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+// imports the built-in Node.js module request (File System)
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+
+    console.log(error || JSON.parse(body).title);
+});
